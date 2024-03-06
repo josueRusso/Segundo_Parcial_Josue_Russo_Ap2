55478f9d92589ee8fa5747c0f971e85a23a33dd5
josueRusso <josue_russo@ucne.edu.do>
2024-03-06 18:28:50 -0400

    Terminando el ClienteRepository

--- app/src/main/java/com/ucne/segundoparcial/data/repository/ClienteRepository.kt
+++ app/src/main/java/com/ucne/segundoparcial/data/repository/ClienteRepository.kt
@@ -1,4 +1,17 @@
 package com.ucne.segundoparcial.data.repository
 
-class ClienteRepository {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity("cliente")
+data class ClienteRepository(
+    @PrimaryKey(autoGenerate = true)
+    val codigoCliente: Int = 0,
+    val nombres: String = "",
+    val direccion: String = "",
+    val telefono: String = "",
+    val celular: String = "",
+    val cedula: String = "",
+    val tipoComprobante: Int = 0,
+)
