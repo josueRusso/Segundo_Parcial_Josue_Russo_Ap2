7393ed162048daa5f1b4a3f8fd4c363bb8b3b9ac
josueRusso <josue_russo@ucne.edu.do>
2024-03-06 18:45:41 -0400

    Terminando el ClienteDataBase

--- app/src/main/java/com/ucne/segundoparcial/data/dataBase/ClienteDataBase.kt
+++ app/src/main/java/com/ucne/segundoparcial/data/dataBase/ClienteDataBase.kt
@@ -1,4 +1,12 @@
 package com.ucne.segundoparcial.data.dataBase
 
-class ClienteDataBase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.ucne.segundoparcial.data.dao.ClienteDao
+import com.ucne.segundoparcial.data.repository.ClienteRepository
+
+
+@Database(entities = [ClienteRepository::class], version = 1)
+abstract class ClienteDataBase : RoomDatabase(){
+    abstract fun ClienteDao(): ClienteDao
 }
